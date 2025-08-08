177e7b57d5420886e5b60b70c914519b6e7d04d6
MichaelF102 <123481426+MichaelF102@users.noreply.github.com>
2025-08-08 15:34:19 +0530

    Update cia1.py

--- cia1.py
+++ cia1.py
@@ -49,7 +49,7 @@ if section == "General":
         <strong>Profilers Used:</strong> cPython, VirtualVM, Valgrind, gprof,pprof
     </p>
     """, unsafe_allow_html=True)
-    st.image("gonb3.png", 
+    st.image("Images/gonb3.png", 
              caption="Languages Benchmarked", 
              use_column_width=True)
 
@@ -3720,4 +3720,5 @@ if section == "Conclusion":
 st.markdown(section_content[section] if section_content[section] else "")
 
 st.sidebar.write("Developed by Michael Fernandes")
+
 
